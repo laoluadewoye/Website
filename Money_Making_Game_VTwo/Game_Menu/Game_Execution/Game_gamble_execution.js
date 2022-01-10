f7332857e738dd2e537492a4bec2812f94c6d266
laoluadewoye <96112707+laoluadewoye@users.noreply.github.com>
2022-01-09 22:30:15 -0500

    Update Game_gamble_execution.js
    Added a set of checks to the boxing function. Fixed the BIGONE option, so there is no way of getting a 0% chance, which is a guaranteed fail (only thing worse than close to guaranteed fail).

--- Money_Making_Game_VTwo/Game_Menu/Game_Execution/Game_gamble_execution.js
+++ Money_Making_Game_VTwo/Game_Menu/Game_Execution/Game_gamble_execution.js
@@ -52,45 +52,81 @@ function boxing() {
   let tempbox = document.getElementsByName("FightAmount");
   let box = parseInt(tempbox[0].value);
 
-  //Add input checks
+  //fighter check
+  if (fighter === 1 || fighter === 2) {
+    //Three further checks needed - below 500 check, max check, and bonus below 0 check
+    if (box > 500) {
+      if (box <= onHand) {
+        //Establishing betting sides
+        let team1 = Math.floor(Math.random() * 24);
+        let team2 = 23 - team1;
 
-  //Establishing betting sides
-  let team1 = Math.floor(Math.random() * 24);
-  let team2 = 23 - team1;
+        //Establishing the group pot
+        let groupPot = Math.floor(Math.random() * 27000) + 13000; //A pot from 13k to 40k
 
-  //Establishing the group pot
-  let groupPot = Math.floor(Math.random() * 27000) + 13000; //A pot from 13k to 40k
+        //Win condition to match
+        let winner = Math.floor(Math.random() * 2) + 1; //0 and 1 --> 1 and 2
 
-  //Win condition to match
-  let winner = Math.floor(Math.random() * 2) + 1; //0 and 1 --> 1 and 2
+        //Some dialogue
+        alert(
+          "With your bet placed, you join your team, rooting for boxer " +
+            fighter +
+            " at their corner."
+        );
+        alert("The fight begins at the bell...");
 
-  //Some dialogue
-  alert(
-    "With your bet placed, you join your team, rooting for boxer " +
-      fighter +
-      " at their corner."
-  );
-  alert("The fight begins at the bell...");
-
-  //Calculations
-  if (fighter == winner) {
-    alert("Your boxer has won!");
-    if (winner == 1) {
+        //Calculations
+        if (fighter == winner) {
+          alert("Your boxer has won!");
+          if (winner == 1) {
+            alert(
+              "You and " +
+                team1 +
+                " others won the grand pot of $" +
+                groupPot +
+                "!"
+            );
+            yourShare = Math.ceil(groupPot / team1);
+          } else if (winner == 2) {
+            alert(
+              "You and " +
+                team2 +
+                " others won the grand pot of $" +
+                groupPot +
+                "!"
+            );
+            yourShare = Math.ceil(groupPot / team2);
+          }
+          alert("Your share is $" + yourShare);
+          onHand = onHand + yourShare;
+        } else {
+          alert(
+            "Your boxer has unfortunately lost. You lose your contribution."
+          );
+          onHand = onHand - box;
+        }
+      }
+    } else {
       alert(
-        "You and " + team1 + " others won the grand pot of $" + groupPot + "!"
+        "I am sorry, but I cannot allow such a low pot addition to our sacred sport."
       );
-      yourShare = Math.ceil(groupPot / team1);
-    } else if (winner == 2) {
-      alert(
-        "You and " + team2 + " others won the grand pot of $" + groupPot + "!"
-      );
-      yourShare = Math.ceil(groupPot / team2);
+      if (box <= 0) {
+        alert("Wait a moment, this isn't even anything!");
+        alert("How dare you insult us like this!");
+        alert(
+          "[You are promptly taken out back and dealt with for the insult. Lose 50 dollars and a day."
+        );
+        onHand = onHand - 50;
+      } else {
+        alert("You are escorted away from the boxing event. Lose a day.");
+      }
     }
-    alert("Your share is $" + yourShare);
-    onHand = onHand + yourShare;
   } else {
-    alert("Your boxer has unfortunately lost. You lose your contribution.");
-    onHand = onHand - box;
+    alert(
+      "There are no other figters. Disappointed fighter " +
+        fighter +
+        " is not there, you pack it up and leave. Lose a day being bummed."
+    );
   }
 }
 
@@ -113,7 +149,7 @@ function bigone() {
     //Valid input check
     if (one >= onHand * 0.75) {
       //75% min check
-      let bigChance = Math.floor(Math.random() * 6);
+      let bigChance = Math.floor(Math.random() * 5) + 1;
       alert("You have a " + bigChance + "% chance of getting this!");
 
       let trueBigChance = Math.floor(Math.random() * 101); //Pick a number between 1 and 100
