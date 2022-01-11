fe442db43fb016f6fa4b6d974813a134d3c41b93
laoluadewoye <96112707+laoluadewoye@users.noreply.github.com>
2022-01-11 12:30:53 -0500

    Update Game_menu.js
    Added toggle resets for main buttons and gamble buttons, no more need for unnecessary scrolling.

--- Money_Making_Game_VTwo/Game_Menu/Game_menu.js
+++ Money_Making_Game_VTwo/Game_Menu/Game_menu.js
@@ -1,7 +1,25 @@
-//Holds all the toggling for each Layer
+//Holds all the toggling for each Layer plus reset functions
+
+//Turns off main layers
+function resettoggle() {
+  let wd = document.getElementById("withdrawLayer");
+  let de = document.getElementById("depositLayer");
+  let tf = document.getElementById("transferLayer");
+  let gam = document.getElementById("gambleLayer");
+  let wk = document.getElementById("workLayer");
+  let sv = document.getElementById("savingsLayer");
+
+  wd.style.display = "none";
+  de.style.display = "none";
+  tf.style.display = "none";
+  gam.style.display = "none";
+  wk.style.display = "none";
+  sv.style.display = "none";
+}
 
 //Beginning of Main Button Toggling
 function togglewithdraw() {
+  resettoggle();
   let x = document.getElementById("withdrawLayer");
   if (x.style.display === "none") {
     x.style.display = "block";
@@ -11,6 +29,7 @@ function togglewithdraw() {
 }
 
 function toggledeposit() {
+  resettoggle();
   let x = document.getElementById("depositLayer");
   if (x.style.display === "none") {
     x.style.display = "block";
@@ -20,6 +39,7 @@ function toggledeposit() {
 }
 
 function toggletransfer() {
+  resettoggle();
   let x = document.getElementById("transferLayer");
   if (x.style.display === "none") {
     x.style.display = "block";
@@ -29,6 +49,7 @@ function toggletransfer() {
 }
 
 function togglegamble() {
+  resettoggle();
   let x = document.getElementById("gambleLayer");
   if (x.style.display === "none") {
     x.style.display = "block";
@@ -38,6 +59,7 @@ function togglegamble() {
 }
 
 function togglework() {
+  resettoggle();
   let x = document.getElementById("workLayer");
   if (x.style.display === "none") {
     x.style.display = "block";
@@ -47,6 +69,7 @@ function togglework() {
 }
 
 function togglesavings() {
+  resettoggle();
   let x = document.getElementById("savingsLayer");
   if (x.style.display === "none") {
     x.style.display = "block";
@@ -56,8 +79,22 @@ function togglesavings() {
 }
 //Ending of Main Button Toggling
 
+//Turns off gamble layers
+function resetgambletoggle() {
+  let gd = document.getElementById("gambleDouble");
+  let gb = document.getElementById("gambleBlackjack");
+  let gf = document.getElementById("gambleFight");
+  let gbo = document.getElementById("gambleBigOne");
+
+  gd.style.display = "none";
+  gb.style.display = "none";
+  gf.style.display = "none";
+  gbo.style.display = "none";
+}
+
 //Beginning of Gamble Button Toggling
 function toggledouble() {
+  resetgambletoggle();
   let x = document.getElementById("gambleDouble");
   if (x.style.display === "none") {
     x.style.display = "block";
@@ -67,6 +104,7 @@ function toggledouble() {
 }
 
 function toggleblackjack() {
+  resetgambletoggle();
   let x = document.getElementById("gambleBlackjack");
   if (x.style.display === "none") {
     x.style.display = "block";
@@ -76,6 +114,7 @@ function toggleblackjack() {
 }
 
 function toggleboxing() {
+  resetgambletoggle();
   let x = document.getElementById("gambleFight");
   if (x.style.display === "none") {
     x.style.display = "block";
@@ -85,6 +124,7 @@ function toggleboxing() {
 }
 
 function togglebigone() {
+  resetgambletoggle();
   let x = document.getElementById("gambleBigOne");
   if (x.style.display === "none") {
     x.style.display = "block";
